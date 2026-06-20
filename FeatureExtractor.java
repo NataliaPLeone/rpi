135c7d0640bcf04d49ca0bdc477ed828fa9a4d6f
NataliaPLeone <119025431+NataliaPLeone@users.noreply.github.com>
2026-06-20 20:53:58 -0300

    Implement FeatureExtractor class for shape analysis

---
+++ FeatureExtractor.java
@@ -0,0 +1,43 @@
+
+
+public class FeatureExtractor {
+    public static FeatureVector extractFeatures(ShapeData shapeData, String imageName) {
+        // Cálculo do diâmetro efetivo
+        double effectiveDiameter = calculateEffectiveDiameter(shapeData);
+
+        // Cálculo da circularidade
+        double circularity = calculateCircularity(shapeData);
+
+        // Cálculo da arredondamento
+        double roundness = calculateRoundness(shapeData);
+
+        // Cálculo da razão de raios
+        double radiusRatio = calculateRadiusRatio(shapeData);
+
+        return new FeatureVector(imageName, effectiveDiameter, circularity, roundness, radiusRatio);
+    }
+
+    private static double calculateEffectiveDiameter(ShapeData shapeData) {
+        double d = 2 * Math.sqrt(shapeData.getArea() / Math.PI);
+
+        return d;
+    }
+
+    private static double calculateCircularity(ShapeData shapeData) {
+        double c = (4 * Math.PI * shapeData.getArea()) / (Math.pow(shapeData.getPerimeter(), 2));
+
+        return c;
+    }
+
+    private static double calculateRoundness(ShapeData shapeData) {
+        double r = (4 * shapeData.getArea()) / (Math.PI * Math.pow(shapeData.getMajorAxis(), 2));
+
+        return r;
+    }
+
+    private static double calculateRadiusRatio(ShapeData shapeData) {
+        double rr = shapeData.getMinorAxis() / shapeData.getMajorAxis();
+
+        return rr;
+    }
+}
